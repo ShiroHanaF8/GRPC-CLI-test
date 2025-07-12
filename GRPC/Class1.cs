3aa94e28e460d2c0b270df5ca92c3f839cc8da13
ShiroHanaF8 <53520538+ShiroHanaF8@users.noreply.github.com>
2025-07-12 21:18:50 +0900

    Add Dialog app / Enable SEH exception

--- GRPC/Class1.cs
+++ GRPC/Class1.cs
@@ -1,4 +1,6 @@
-﻿namespace GRPC
+﻿using MessagePack;
+
+namespace GRPC
 {
     public class Class1
     {
@@ -7,17 +9,9 @@
             return new Random((int)DateTime.Now.Ticks).Next();
         }
 
-        public Grpc.Core.KeyCertificatePair GetKeyCertificatePair()
+        public MessagePack.MessagePackType GetMessagePackType()
         {
-            return new Grpc.Core.KeyCertificatePair(
-                "-----BEGIN CERTIFICATE-----\n" +
-                "MIIDXTCCAkWgAwIBAgIJAL5k1Z3b4z2wMA0GCSqGSIb3DQEBCwUAMEUxCzAJBgNV\n" +
-                "BAYTAlVTMRMwEQYDVQQIEwpDYWxpZm9ybmlhMRAwDgYDVQQHEwdTYW4gSm9zZTET\n" +
-                "MBEGA1UEChMKVGVzdCBDb21wYW55MB4XDTIxMDYyMjA5MDAwMFoXDTMxMDYyMjA5\n" +
-                "MDAwMFowRTELMAkGA1UEBhMCVVMxEzARBgNVBAgTCkNhbGlmb3JuaWExEDAOBgNV\n",
-                "BAcTB1NhbiBKb3NlMRMwEQYDVQQKEwpUZXN0IENvbXBhbnkwggEiMA0GCSqGSIb3\n" +
-                "DQEBAQUAA4IBDwAwggEKAoIBAQCy7d8f8+6v7d8f8+6v7d8f8+6v7d8f8+6v7d8f\n"
-                );
+            return MessagePack.MessagePackType.Integer;
         }
     }
 }
---
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -0,0 +1,98 @@
+﻿
+// MFCApplication1Dlg.cpp : 実装ファイル
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+
+#include "WrapperClass.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication1Dlg ダイアログ
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT1, m_textEdit);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+END_MESSAGE_MAP()
+
+
+// CMFCApplication1Dlg メッセージ ハンドラー
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// このダイアログのアイコンを設定します。アプリケーションのメイン ウィンドウがダイアログでない場合、
+	//  Framework は、この設定を自動的に行います。
+	SetIcon(m_hIcon, TRUE);			// 大きいアイコンの設定
+	SetIcon(m_hIcon, FALSE);		// 小さいアイコンの設定
+
+	CString text, tmp;
+	text += "Press Enter to continue...\n";
+	text += "Rand:";
+	tmp.Format(_T("%d\n"), WrapperClass::WrapperFunction1());
+	text += tmp;
+	text += "KeyCertPair: ";
+	text += WrapperClass::WrapperFunction2().c_str();
+	m_textEdit.SetWindowTextW(text);
+
+	return TRUE;  // フォーカスをコントロールに設定した場合を除き、TRUE を返します。
+}
+
+// ダイアログに最小化ボタンを追加する場合、アイコンを描画するための
+//  下のコードが必要です。ドキュメント/ビュー モデルを使う MFC アプリケーションの場合、
+//  これは、Framework によって自動的に設定されます。
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 描画のデバイス コンテキスト
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// クライアントの四角形領域内の中央
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// アイコンの描画
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// ユーザーが最小化したウィンドウをドラッグしているときに表示するカーソルを取得するために、
+//  システムがこの関数を呼び出します。
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
